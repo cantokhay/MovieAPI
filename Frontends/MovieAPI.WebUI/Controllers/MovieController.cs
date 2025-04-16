9e7726d54a309cdb3cfc596fde2cc6d6dd0f00bd
Can TOKHAY <cantokhay@outlook.com>
2025-04-16 13:26:17 +0300

    End of 27

---
+++ Frontends/MovieAPI.WebUI/Controllers/MovieController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieAPI.WebUI.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult MovieList()
+        {
+            ViewBag.v1 = "Movies - List";
+            ViewBag.v2 = "ABC";
+            ViewBag.v3 = "movies list";
+            return View();
+        }
+    }
+}
