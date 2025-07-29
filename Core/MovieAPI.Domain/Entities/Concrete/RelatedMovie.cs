807226c4786d116144045c9752ab6963c79d356a
cantokhay <cantokhay@gmail.com>
2025-07-29 14:22:10 +0300

    End of 32

---
+++ Core/MovieAPI.Domain/Entities/Concrete/RelatedMovie.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieAPI.Domain.Entities.Concrete
+{
+    public class RelatedMovie
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public int UserId { get; set; }
+        public bool IsWatched { get; set; }
+    }
+}
